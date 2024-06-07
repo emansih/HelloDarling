0a1ad780aeeb764d94af20433bbcda7043a84c65
emansih <danielquah148@yahoo.com>
2024-06-07 15:59:36 +1000

    Allow admin to book dates between 2 users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from './components/Dashboard';
 import User from './components/User';
+import DateBooking from './components/DateBooking';
 
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
           <div className="App">
           <Routes>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/booking" element={<DateBooking />} />
             <Route path="/user/:userId" element={<User />} />
           </Routes>
           </div>
---
+++ frontend/src/components/DateBooking.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import Form from "react-bootstrap/Form";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
+const DateBooking = () => {
+    const [users, setUsers] = useState([]);
+    const [modalMessage, setModalMessage] = useState('');
+    const [showModal, setShowModal] = useState(false);
+    const [formData, setFormData] = useState({
+        daterOne: '',
+        daterTwo: '',
+        startDate: '',
+        startTime: '',
+        endTime: '',
+        dateLocation: ''
+    });
+
+    const handleInputChange = (e) => {
+        const { id, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [id]: value
+        }));
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { startDate, startTime, endTime, ...rest } = formData;
+        const combinedStartDateTime = `${formData.startDate}T${formData.startTime}:00.000+00:00`;
+        // Assuming the end date is the same as the start date
+        const combinedEndDateTime = `${formData.startDate}T${formData.endTime}:00.000+00:00`; 
+        const submissionData = {
+            ...rest,
+            dateStart: combinedStartDateTime,
+            dateEnd: combinedEndDateTime
+        };
+        axios.post('http://localhost:3000/api/v1/users/booking', submissionData)
+            .then(response => {
+                setModalMessage(response.data.message);
+                setShowModal(true);
+
+            })
+            .catch(error => {
+                setModalMessage(error.response.data.message);
+                setShowModal(true);
+            });
+    };
+
+    useEffect(() => {
+        const client = axios.create({
+            baseURL: "http://localhost:3000/api/v1" 
+        });
+        client.get('/users/').then((response) => {
+            setUsers(response.data);
+        });
+    }, []);
+
+    return (
+        <>
+
+        <Form onSubmit={handleSubmit}>
+            All date times are in UTC. 
+            <h1>Date 1</h1>
+            <label>
+                <select id="daterOne" value={formData.daterOne} onChange={handleInputChange}>
+                    <option value="" disabled>Select User</option>
+                    {users.map(user => (
+                        <option key={user.userId} value={user.userId}>{user.firstName} {user.lastName}</option>
+                    ))}
+                </select>
+            </label>
+            
+            <h1>Date 2</h1>
+            <label>
+                <select id="daterTwo" value={formData.daterTwo} onChange={handleInputChange}>
+                    <option value="" disabled>Select User</option>
+                    {users.map(user => (
+                        <option key={user.userId} value={user.userId}>{user.firstName} {user.lastName}</option>
+                    ))}
+                </select>
+            </label>
+            <br />
+            <br />
+            <Form.Control id="startDate" type="date" value={formData.startDate} onChange={handleInputChange} required />
+            <br />
+            <br />
+            Time Start:
+            <br />  
+            <Form.Control id="startTime" type="time" value={formData.startTime} onChange={handleInputChange} required />
+            <br />
+            <br />
+            Time End:  
+            <br />
+            <Form.Control id="endTime" type="time" value={formData.endTime} onChange={handleInputChange} required />
+            <br />
+            <br />
+            Location: 
+            <br />
+            <br />
+            <Form.Control id="dateLocation" type="text" value={formData.dateLocation} onChange={handleInputChange} required />
+            <br />
+            <br />
+            <Button type="submit" variant="primary">Submit</Button>
+        </Form>
+
+        <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Submission Status</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{modalMessage}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+};
+
+export default DateBooking;
